69f97ec4fcd02fb64f864f17b7687dab10c3f3b8
romantic <1095747585@qq.com>
2018-11-21 19:14:20 +0800

    commit

---
+++ YFAlertView/YFAlertView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  YFAlertView
+//
+//  Created by YangFeng on 2018/11/21.
+//  Copyright © 2018年 YangFeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ YFAlertView/YFAlertView/ViewController.m
@@ -0,0 +1,23 @@
+//
+//  ViewController.m
+//  YFAlertView
+//
+//  Created by YangFeng on 2018/11/21.
+//  Copyright © 2018年 YangFeng. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+@end
